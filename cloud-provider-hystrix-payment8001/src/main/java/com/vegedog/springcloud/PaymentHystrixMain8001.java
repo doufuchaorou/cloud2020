1a91e618b52a2b6b81502d26d5be77d513335237
Vegedog <545967173@qq.com>
2020-03-19 00:32:30 +0800

    Hystrix支付微服务构建
    Signed-off-by: Vegedog <545967173@qq.com>

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/vegedog/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,19 @@
+package com.vegedog.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @program: cloud2020
+ * @description
+ * @author: Vegedog
+ * @create: 2020-03-18 22:50
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+}
