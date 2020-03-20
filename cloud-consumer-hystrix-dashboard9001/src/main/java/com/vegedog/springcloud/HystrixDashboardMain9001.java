074deaa514dcdccc931317f9a728c585b247b901
Vegedog <545967173@qq.com>
2020-03-20 11:42:03 +0800

    Hystrix图形化Dashboard搭建与监控实战
    Signed-off-by: Vegedog <545967173@qq.com>

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/vegedog/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.vegedog.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @program: cloud2020
+ * @description
+ * @author: Vegedog
+ * @create: 2020-03-20 10:55
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
