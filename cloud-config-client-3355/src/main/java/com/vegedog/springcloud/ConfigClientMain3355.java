2dd4f60ac7426b16019a3c0a1d572428b0105cc7
Vegedog <545967173@qq.com>
2020-03-21 20:30:45 +0800

    SpringCloud Config分布式配置

---
+++ cloud-config-client-3355/src/main/java/com/vegedog/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.vegedog.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @program: cloud2020
+ * @description
+ * @author: Vegedog
+ * @create: 2020-03-21 16:13
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
