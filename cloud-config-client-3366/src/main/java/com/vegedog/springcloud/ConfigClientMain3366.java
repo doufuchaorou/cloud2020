71d57fc60661fd0c5f87060cada119a2f1dcdac9
Vegedog <545967173@qq.com>
2020-03-31 17:57:10 +0800

    Bus动态刷新
    Signed-off-by: Vegedog <545967173@qq.com>

---
+++ cloud-config-client-3366/src/main/java/com/vegedog/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.vegedog.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @program: cloud2020
+ * @description
+ * @author: Vegedog
+ * @create: 2020-03-21 21:47
+ **/
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/vegedog/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,29 @@
+package com.vegedog.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: cloud2020
+ * @description
+ * @author: Vegedog
+ * @create: 2020-03-21 17:13
+ **/
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort" + serverPort + "\t\n\n" + "configInfo" + configInfo;
+    }
+
+}
